b839609be200e58bd48521d95906c6670fcad895
Michael <example@gmail.com>
2020-05-14 19:15:36 +0300

    Создал первое консольное приложение

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const numberOfFilms = +prompt('Сколько фильмоы вы уже посмотрели?', '');
+
+const presonalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', '');   
+const b = prompt('На сколько его оцените?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько его оцените?', '');
+
+presonalMovieDB.movies[a] = b;
+presonalMovieDB.movies[c] = d;
+
+console.log(presonalMovieDB);
